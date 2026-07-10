b141c85a3195c1fc9970bee0d4e923f4783dbffc
Bryant Mejia <bmejia220@gmail.com>
2026-07-10 19:43:38 -0400

    chore: remove unused nap-test-utils crate

--- crates/nap-test-utils/src/mock_repository.rs
+++
@@ -1,7 +0,0 @@
-use crate::mock_backend::MockBackend;
-use nap_core::repository::Repository;
-use tempfile::TempDir;
-
-pub fn mock_repo(tmp: &TempDir) -> Repository {
-    Repository::init(tmp.path(), "testverse", Box::new(MockBackend::new())).unwrap()
-}
